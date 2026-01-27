12b0e2a97c6675acee5a0b632ff45adabee33d63
Sham Voke <shamvoke@webmeka.com>
2026-01-28 00:54:26 +0300

    typewriter

--- src/js/main/shamtext.js
+++ src/js/main/shamtext.js
@@ -1,19 +1,24 @@
 const phrases = [
-    "➤ All cursors have a point! Let that click.",
-    "➤ Crafting pixels into art, one design at a time.",
-    "➤ Kindness is free, sprinkle that stuff everywhere.",
-    "➤ Creativity is just intelligence having fun."
+  "All cursors have a point! Let that click.",
+  "Crafting pixels into art, one design at a time.",
+  "Kindness is free, sprinkle that stuff everywhere.",
+  "Creativity is just intelligence having fun."
 ];
 
-function updateText() {
-    const randomIndex = Math.floor(Math.random() * phrases.length);
-    const randomPhrase = phrases[randomIndex];
+const target = document.getElementById("shamtext");
 
-    const textElement = document.getElementById('shamtext');
+const typewriter = new Typewriter(target, {
+  loop: true,
+  delay: 55,
+  deleteSpeed: 30
+});
 
-    if (textElement) {
-        textElement.textContent = randomPhrase;
-    }
-}
-updateText();
-setInterval(updateText, 5200);
+phrases.forEach(text => {
+  typewriter
+    .typeString(text)
+    .pauseFor(1800)
+    .deleteAll()
+    .pauseFor(400);
+});
+
+typewriter.start();
