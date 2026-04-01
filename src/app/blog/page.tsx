b043536f6ce2f2ad9b04cbabd2a8144a26d3159b
sham voke <shamvoke@webmeka.com>
2026-04-01 13:13:42 +0300

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -12,8 +12,8 @@ export default function BlogIndex() {
 
   return (
     <main className="mx-auto sm:px-6 lg:px-8 py-20">
-      <section className="mb-12 md:mb-16 px-8">
-        <p className="text-sm uppercase tracking-[0.2em] font-voke text-sham dark:text-voke mb-3">
+      <section className="mb-12 md:mb-16 px-8 text-center">
+        <p className="text-2xl tracking-[0.2em] font-voke text-sham dark:text-voke">
           Shamthing to read...
         </p>
         <p className="mt-4 max-w-2xl text-base md:text-lg text-neutral-600 dark:text-neutral-400 leading-8">
