442c497e01056ecdda35d92ae75ed63a2eb9c3f0
Sham Voke <shamvoke@webmeka.com>
2026-05-21 10:07:47 +0300

    reading pill

--- src/app/blog/PostsGrid.tsx
+++ src/app/blog/PostsGrid.tsx
@@ -39,7 +39,7 @@ export default function PostsGrid({ posts }: PostsGridProps) {
                 <div className="absolute inset-0 bg-black/0 group-hover:bg-black/10 transition duration-300" />
 
                 <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100">
-                <div className="flex items-center justify-center w-16 h-16 rounded-full border-2 bg-black/20 backdrop-blur-sm">
+                <div className="flex items-center justify-center w-16 h-8 rounded-xl border-2 bg-black/20 backdrop-blur-xs">
                     <span className="text-voke text-sm font-semibold">Read</span>
                 </div>
                 </div>
