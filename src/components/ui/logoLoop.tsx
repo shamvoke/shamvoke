f68acdcb32a8fde4373a8c028bfcc0ffc37e6285
Sham Voke <shamvoke@webmeka.com>
2026-05-27 11:59:05 +0300

    Update logoLoop.tsx

--- src/components/ui/logoLoop.tsx
+++ src/components/ui/logoLoop.tsx
@@ -14,12 +14,7 @@ export default function LogoLoop({
   const items = [...logos, ...logos]
 
   return (
-    <section className="relative w-full pt-15 overflow-hidden">
-      <div className="mb-8 px-6 text-center">
-        <h2 className="text-2xl font-semibold font-sham sm:font-sans tracking-tight text-black dark:text-white sm:text-4xl md:text-5xl">
-          MEET BREE
-        </h2>
-      </div>
+    <section className="relative w-full py-15 overflow-hidden">
       <div className="pointer-events-none absolute inset-y-0 left-0 z-10 w-20 bg-linear-to-r from-background to-transparent" />
       <div className="pointer-events-none absolute inset-y-0 right-0 z-10 w-20 bg-linear-to-l from-background to-transparent" />
 
