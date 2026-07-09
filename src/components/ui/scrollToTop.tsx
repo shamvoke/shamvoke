559cdb46299a1f0ea3f972dcca5e36f044589c95
Sham Voke <shamvoke@webmeka.com>
2026-07-09 04:40:49 -0400

    Update scrollToTop.tsx

--- src/components/ui/scrollToTop.tsx
+++ src/components/ui/scrollToTop.tsx
@@ -22,7 +22,7 @@ export default function ScrollToTop() {
   return (
     <button
       onClick={scrollToTop}
-      className={`fixed bottom-6 right-6 z-50 rounded-full border-2 font-bold bg-neutral-950/10 backdrop-blur-md p-3 text-white shadow-lg transition-all duration-300
+      className={`fixed bottom-6 right-6 z-50 rounded-full border-2 font-bold bg-neutral-950/10 backdrop-blur-md p-3  shadow-lg transition-all duration-300
         ${visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4 pointer-events-none"}`}
       aria-label="Scroll to top"
     >
