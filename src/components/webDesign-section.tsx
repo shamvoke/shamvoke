8a2535edc9fbcbd28e4778d33084aaa6bb58b157
Sham Voke <shamvoke@webmeka.com>
2026-09-10 22:33:05 +0300

    Update webDesign-section.tsx

--- src/components/webDesign-section.tsx
+++ src/components/webDesign-section.tsx
@@ -28,7 +28,7 @@ export default function WebDesignSection() {
 
         <div className="mx-auto max-w-3xl text-center">
           <h2 className="text-2xl font-semibold font-sham sm:font-sans tracking-tight text-black dark:text-white sm:text-4xl md:text-4xl">
-            Like this site? I build personal spaces like this.
+            Like this site? Get your personal space like this.
           </h2>
           <p className="mt-5 text-sm font-bold leading-7 text-black dark:text-white/60 sm:text-base">
             From personal blogs to business websites, I help you bring your vision to life.
